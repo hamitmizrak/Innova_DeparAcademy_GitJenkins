6059f4127109e5b90f9d922845cc6ff0e65d9eb9
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-06-17 11:51:10 +0300

    Java compiler

---
+++ JenkinsJavaTutorials.java
@@ -0,0 +1,9 @@
+package com.hamitmizrak.main;
+
+public class JenkinsJavaTutorials {
+    public static void main(String[] args) {
+        System.out.println("Merhabalar Ben GitHub gelen Compile edilen bir classım");
+
+        //JenkinsJavaTutorials.java
+    }
+}
---
+++ src/main/java/com/hamitmizrak/main/JenkinsJavaTutorials.java
@@ -0,0 +1,7 @@
+package com.hamitmizrak.main;
+
+public class JenkinsJavaTutorials {
+    public static void main(String[] args) {
+        System.out.println("Merhabalar Ben GitHub gelen Compile edilen bir classım");
+    }
+}
