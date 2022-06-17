d42a8e8f1105d48facf53b8c59911e1c34a01025
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-06-17 13:45:30 +0300

    Deneme

---
+++ Deneme.java
@@ -0,0 +1,8 @@
+
+public class Deneme {
+    public static void main(String[] args) {
+        System.out.println("Merhabalar Ben GitHub gelen Compile edilen bir classım");
+
+        //JenkinsJavaCompiler.java
+    }
+}
