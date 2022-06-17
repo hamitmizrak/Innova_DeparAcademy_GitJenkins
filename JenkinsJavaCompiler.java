6ac323547f9a5fe4b18af68a8e93e31169325264
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-06-17 12:12:32 +0300

    ClassName

--- JenkinsJavaCompiler.java
+++ JenkinsJavaCompiler.java
@@ -3,6 +3,6 @@ public class JenkinsJavaTutorials {
     public static void main(String[] args) {
         System.out.println("Merhabalar Ben GitHub gelen Compile edilen bir classım");
 
-        //JenkinsJavaTutorials.java
+        //JenkinsJavaCompiler.java
     }
 }
